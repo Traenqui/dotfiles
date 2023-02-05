e60e7f2e168803dd26e32a5045bf0726f59178dc
Jonas Gerber <j.gerber2803@gmail.com>
2023-02-05 18:12:03 +0100

    docs(gitignore): Added zsh_istory to git ignore

--- mac/brew.sh
+++ mac/brew.sh
@@ -4,11 +4,20 @@
 brew install iterm2
 brew install starship
 brew install gh
+brew install httpie
+brew install bottom
+brew install rs/tap/curlie
+brew install bat
+brew install exa
+brew install neovim
 
 brew install --cask 1password
 brew install --cask obsidian
 brew install --cask visual-studio-code
+brew install --cask docker
+brew install --cask iterm2
 brew install --cask google-chrome
+brew install --cask onedrive
 brew install --cask microsoft-word
 brew install --cask microsoft-excel
 brew install --cask microsoft-outlook
